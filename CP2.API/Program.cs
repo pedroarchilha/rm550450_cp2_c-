1c0dc8155eb874ed05b424eba6a15f24d1123907
pedroarchilha <rm550450@fiap.com.br>
2024-10-10 23:03:34 -0300

    Commit com o projeto CP2 de C#

---
+++ CP2.API/Program.cs
@@ -0,0 +1,28 @@
+using CP2.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+Bootstrap.Start(builder.Services, builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ CP2.Application/Dtos/FornecedorDto.cs
@@ -0,0 +1,34 @@
+﻿using CP2.Domain.Interfaces.Dtos;
+using FluentValidation;
+using System;
+
+namespace CP2.Application.Dtos
+{
+    public class FornecedorDto : IFornecedorDto
+    {
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public DateTime CriadoEm { get; set; }
+
+        public void Validate()
+        {
+            var validateResult = new FornecedorDtoValidation().Validate(this);
+
+            if (!validateResult.IsValid)
+                throw new Exception(string.Join(" e ", validateResult.Errors.Select(x => x.ErrorMessage)));
+        }
+    }
+
+    internal class FornecedorDtoValidation : AbstractValidator<FornecedorDto>
+    {
+        public FornecedorDtoValidation()
+        {
+            RuleFor(x => x.Nome).NotEmpty().WithMessage("O nome nao pode ficar vazio.");
+            RuleFor(x => x.CNPJ).NotEmpty().WithMessage("O CNPJ nao pode ficar vazio.");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email inserido esta errado.");
+        }
+    }
+}
